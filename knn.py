b466d4187cba6247248de5d32017dead7937a577
zhai_pro <xin_soft@foxmail.com>
2015-03-26 20:05:24 +0800

    16k样本，1.6k中心，简化的ISODATA算法

--- knn.py
+++ knn.py
@@ -3,34 +3,42 @@
 import cv2
 import numpy as np
 import os
+import time
 
 
 class Knn:
     def __init__(self, k):
         self.k = k
-        self.cs = np.random.random((k, 1083))
-        # 一下变量用于计算新的中心
-        self._cs = np.zeros((k, 1083))
-        self._count = np.zeros(k)
+        self.cs = np.zeros((k, 1083))
+        self.count = np.zeros(k)     # 距离相应中心最近的点的个数
 
     def train(self, X):
-        self.cs = X[0: self.k]
-        for t in xrange(50):
-            print 'iter', t
-            for x in X:
-                i = np.argmin([np.linalg.norm(x-c) for c in self.cs])
-                self._cs[i] += x
-                self._count[i] += 1
-            for i in xrange(self.k):
-                if self._count[i] != 0:
-                    self._cs[i] /= self._count[i]
+        last_cs = X[0: self.k]  # or np.random.random((k, 1083))
 
-            if sum((np.linalg.norm(x) for x in self._cs - self.cs)) < 0.05:
+        for t in xrange(50):
+            print 'iter', t, 'time', time.clock()
+            self.cs = np.zeros((self.k, 1083))
+            self.count = np.zeros(self.k)
+            for x in X:
+                i = np.argmin([np.linalg.norm(x-c) for c in last_cs])
+                self.cs[i] += x
+                self.count[i] += 1
+            for i in xrange(self.k):
+                if self.count[i] != 0:
+                    self.cs[i] /= self.count[i]
+
+            move_len = sum((np.linalg.norm(x) for x in last_cs - self.cs))
+            print 'move len', move_len
+            if move_len < 0.05:
                 break
 
-            self.cs = self._cs
-            self._cs = np.zeros((self.k, 1083))
-            self._count = np.zeros(self.k)
+            s = self.count.argsort()
+            l = len([t for t in self.count if t == 0])
+            print 'no point cs count', l
+            print self.cs[s[self.k-1]]
+            for i in xrange(l):
+                self.cs[s[i]] = self.cs[s[self.k-i-1]] + np.random.random(1083)/10
+            last_cs = self.cs
 
     def classify(self, x):
         return np.argmin([np.linalg.norm(x-c) for c in self.cs])
@@ -42,10 +50,10 @@ def get_img_as_vector(fn):
     (retval, dst) = cv2.threshold(im, 128, 1, cv2.THRESH_BINARY_INV)
     return dst.reshape(dst.size)
 
-k = Knn(1000)    # 分1000个类别
+k = Knn(1600)    # 分1000个类别
 fns = os.listdir('ocr')
-train_fns = fns[0: 10000]
-test_fns = fns[10000: ]
+train_fns = fns[0: 16000]
+test_fns = fns[16000: ]
 # 读取训练用数据
 print 'Start: read data'
 X = [get_img_as_vector(os.path.join('ocr', fn)) for fn in train_fns]
--- tool.py
+++ tool.py
@@ -1,23 +1,26 @@
 # -*- coding: cp936 -*-
 import os
 import shutil
+import sys
+
+result_fn = sys.argv[1]
+classify_fn = sys.argv[2]
 
 
 # 统计有多少聚类中心是有样本的
-l = [0]*1000
-fp = open('10000测试结果2.txt')
+l = [0]*1600
+fp = open(result_fn)
 for line in fp:
     (fn, classify) = line.strip().split(' ')
     l[int(classify)] += 1
 print len([x for x in l if x>0])
+fp.close()
 
 
-'''
 # 将聚类后的样本复制并使用聚类结果命名
-fp = open('10000测试结果2.txt')
+fp = open(result_fn)
 i = 0
 for line in fp:
     (fn, classify) = line.strip().split(' ')
-    shutil.copy(os.path.join('ocr', fn), 'classify2/%s(%d).jpg' % (classify, i))
+    shutil.copy(os.path.join('ocr', fn), '%s/%s(%d).jpg' % (classify_fn, classify, i))
     i += 1
-'''

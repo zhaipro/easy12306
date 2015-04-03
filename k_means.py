317a23cd613979351acf51aaad0a8c72519f2937
zhai_pro <xin_soft@foxmail.com>
2015-04-03 22:56:49 +0800

    使用PCA进行降维
    保留99%的方差

--- k_means.py
+++ k_means.py
@@ -4,6 +4,7 @@ import cv2
 import os
 import time
 from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 from sklearn.externals import joblib
 
 
@@ -20,17 +21,25 @@ def main():
     fns = os.listdir('ocr')
     X = [get_img_as_vector(os.path.join('ocr', fn)) for fn in fns]
     print 'Samples', len(X), 'Feature', len(X[0])
+    #PCA
+    print 'Start: PCA', time.clock()
+    pca = PCA(n_components=0.99)
+    pca.fit(X)
+    X = pca.transform(X)
+    print 'Samples', len(X), 'Feature', len(X[0])
     # 训练
     print 'Start: train', time.clock()
     n_clusters = 2000    # 聚类中心个数
-    estimator = KMeans(n_clusters, n_init=1, max_iter=20)
+    estimator = KMeans(n_clusters, n_init=1, max_iter=20, verbose=True)
     estimator.fit(X)
     print 'Clusters', estimator.n_clusters, 'Iter', estimator.n_iter_
     print 'Start: classify', time.clock()
+    fp = open('result11.txt', 'w')
     for fn, c in zip(fns, estimator.labels_):
-        print fn, c
+        print >> fp, fn, c
+    fp.close()
     print 'Start: save model', time.clock()
-    joblib.dump(estimator, 'k-means6.pkl')
+    joblib.dump(estimator, 'k-means11.pkl')
 
 if __name__ == '__main__':
     main()

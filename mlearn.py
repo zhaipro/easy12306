89ea61c649f2bf4989a799bfbcc16ab9d3680510
zhai_pro <xin_soft@foxmail.com>
2019-01-07 16:32:10 +0800

    改用卷积神经网络
    真的好兴奋呀

--- mlearn.py
+++ mlearn.py
@@ -13,21 +13,40 @@ def load_data():
 
 
 def main():
+    import matplotlib.pyplot as plt
     from keras import models
     from keras import layers
     (train_x, train_y), (test_x, test_y) = load_data()
     _, train_x = cv2.threshold(train_x, 220, 1, cv2.THRESH_BINARY)
     _, test_x = cv2.threshold(test_x, 220, 1, cv2.THRESH_BINARY)
+    _, h, w = train_x.shape
+    train_x.shape = (-1, h, w, 1)
+    test_x.shape = (-1, h, w, 1)
     model = models.Sequential([
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(h, w, 1)),
+        layers.MaxPooling2D(),  # 19 -> 17 -> 8
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.MaxPooling2D(),  # 8 -> 6 -> 3
         layers.Flatten(),
-        layers.Dense(500, activation='relu'),
+        layers.Dense(256, activation='relu'),
         layers.Dense(80, activation='softmax'),
     ])
-    model.compile(optimizer='adam',
+    model.summary()
+    model.compile(optimizer='rmsprop',
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
-    model.fit(train_x, train_y, epochs=50)
-    print(model.evaluate(test_x, test_y))
+    history = model.fit(train_x, train_y, epochs=30,
+                        validation_data=(test_x, test_y))
+    loss = history.history['loss'][1:]
+    val_loss = history.history['val_loss'][1:]
+    epochs = list(range(2, len(loss) + 2))
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig('loss.jpg')
     model.save('model.h5')
 
 
@@ -36,6 +55,8 @@ def predict():
     model = models.load_model('model.h5')
     texts = np.load('data.npy')
     _, texts = cv2.threshold(texts, 220, 1, cv2.THRESH_BINARY)
+    _, h, w = texts.shape
+    texts.shape = (-1, h, w, 1)
     labels = model.predict(texts)
     np.save('labels.npy', labels)
 

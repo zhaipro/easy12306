78507cbef27955d88a9d14e22b1091c56c435fbf
zhai_pro <xin_soft@foxmail.com>
2019-01-17 20:53:46 +0800

    学习识别有噪音的文字图

--- mlearn.py
+++ mlearn.py
@@ -5,23 +5,39 @@ import cv2
 import numpy as np
 
 
-def load_data():
-    data = np.load('texts.npz')
+def load_data(fn='texts.npz', to=False):
+    from keras.utils import to_categorical
+    data = np.load(fn)
     texts, labels = data['texts'], data['labels']
+    texts = texts / 255.0
+    _, h, w = texts.shape
+    texts.shape = (-1, h, w, 1)
+    if to:
+        labels = to_categorical(labels)
     n = int(texts.shape[0] * 0.9)   # 90%用于训练，10%用于测试
     return (texts[:n], labels[:n]), (texts[n:], labels[n:])
 
 
-def main():
+def savefig(history, fn='loss.jpg'):
     import matplotlib.pyplot as plt
+    # 忽略起点
+    loss = history.history['loss'][1:]
+    val_loss = history.history['val_loss'][1:]
+    epochs = list(range(2, len(loss) + 2))
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig(fn)
+
+
+def main():
     from keras import models
     from keras import layers
     (train_x, train_y), (test_x, test_y) = load_data()
-    train_x = train_x / 255.0
-    test_x = test_x / 255.0
-    _, h, w = train_x.shape
-    train_x.shape = (-1, h, w, 1)
-    test_x.shape = (-1, h, w, 1)
+    _, h, w, _ = train_x.shape
     model = models.Sequential([
         layers.Conv2D(32, (3, 3), activation='relu', input_shape=(h, w, 1)),
         layers.MaxPooling2D(),  # 19 -> 17 -> 8
@@ -38,16 +54,76 @@ def main():
                   metrics=['accuracy'])
     history = model.fit(train_x, train_y, epochs=30,
                         validation_data=(test_x, test_y))
-    loss = history.history['loss'][1:]
-    val_loss = history.history['val_loss'][1:]
-    epochs = list(range(2, len(loss) + 2))
-    plt.plot(epochs, loss, 'bo', label='Training loss')
-    plt.plot(epochs, val_loss, 'b', label='Validation loss')
-    plt.title('Training and validation loss')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.savefig('loss.jpg')
+    savefig(history)
+    model.save('model.h5')
+
+
+def load_data_v2():
+    (train_x, train_y), (test_x, test_y) = load_data(to=True)
+    # 这里是统计学数据
+    (train_v2_x, train_v2_y), (test_v2_x, test_v2_y) = load_data('texts.v2.npz')
+    # 合并
+    train_x = np.concatenate((train_x, train_v2_x))
+    train_y = np.concatenate((train_y, train_v2_y))
+    test_x = np.concatenate((test_x, test_v2_x))
+    test_y = np.concatenate((test_y, test_v2_y))
+    return (train_x, train_y), (test_x, test_y)
+
+
+def acc(y_true, y_pred):
+    import keras.backend as K
+    return K.cast(K.equal(K.argmax(y_true + y_pred, axis=-1),
+                          K.argmax(y_pred, axis=-1)),
+                  K.floatx())
+
+
+def main_v2():
+    from keras import models
+    from keras import layers
+    from keras import optimizers
+    (train_x, train_y), (test_x, test_y) = load_data()
+    _, h, w, _ = train_x.shape
+    model = models.Sequential([
+        layers.Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(h, w, 1)),
+        layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
+        layers.MaxPooling2D(),  # 19 -> 9
+        layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
+        layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
+        layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
+        layers.MaxPooling2D(),  # 9 -> 4
+        layers.Conv2D(128, (3, 3), activation='relu'),
+        layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
+        layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
+        layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
+        layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
+        layers.MaxPooling2D(),  # 4 -> 2
+        layers.Flatten(),
+        layers.Dropout(0.1),
+        layers.Dense(256, activation='relu'),
+        layers.Dense(80, activation='softmax'),
+    ])
+    model.summary()
+    model.compile(optimizer='rmsprop',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    model.fit(train_x, train_y, epochs=10,
+              validation_data=(test_x, test_y))
+    (train_x, train_y), (test_x, test_y) = load_data_v2()
+    model.compile(optimizer=optimizers.RMSprop(lr=1e-4),
+                  loss='categorical_hinge',
+                  metrics=[acc])
+    model.fit(train_x, train_y, epochs=20,
+              validation_data=(test_x, test_y))
+    model.compile(optimizer=optimizers.RMSprop(lr=1e-5),
+                  loss='categorical_hinge',
+                  metrics=[acc])
+    history = model.fit(train_x, train_y, epochs=40, batch_size=10 * 80,
+                        validation_data=(test_x, test_y))
+    savefig(history)
+    # 保存前，先编译回正式版本
+    model.compile(optimizer='rmsprop',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
     model.save('model.h5')
 
 
@@ -80,5 +156,6 @@ def show():
 
 if __name__ == '__main__':
     main()
+    # main_v2()
     _predict()
     show()
